3795ee61bc10f6ab2e5ce1affe57a532d884a83c
Maksim Ermilov <maks.ermilov.03@mail.ru>
2023-02-13 13:21:42 +0300

    Add project files.

---
+++ ThreeplyWebApi/Controllers/SchedulesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThreeplyWebApi.Services;
+using ThreeplyWebApi.Models;
+
+namespace ThreeplyWebApi.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class SchedulesController : ControllerBase
+    {
+        readonly private SchedulesService _schedulesService;
+        public SchedulesController(SchedulesService schedulesService)
+        {
+            _schedulesService = schedulesService;
+        }
+        [HttpGet]
+        public async Task<List<Schedule>> Get() => await _schedulesService.GetAsync();
+        [HttpGet("{groupName}")]
+        public async Task<ActionResult<Schedule>> Get(string groupName)
+        {
+            var schedule = await _schedulesService.GetAsync(groupName);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+            return Ok(schedule);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Schedule newSchedule)
+        {
+            await _schedulesService.CreateAsync(newSchedule);
+            return CreatedAtAction(nameof(Get), new { groupName = newSchedule.groupName }, newSchedule);
+        }
+    }
+}
---
+++ ThreeplyWebApi/Models/ScheduleDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ThreeplyWebApi.Models
+{
+    public class ScheduleDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+        public string DatabaseName { get; set; } = null!;
+        public string SchedulesCollectionName { get; set; } = null!;
+    }
+}
